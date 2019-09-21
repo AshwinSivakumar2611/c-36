d44bd2b72d26accd280368ad18db63ee3ce83c8e
AshwinSivakumar2611 <ashwinwins2019@gmail.com>
2019-09-21 11:44:05 +0530

    c36

--- js/Game.js
+++ js/Game.js
@@ -4,8 +4,8 @@ class Game {
   getState(){
     var gameStateRef  = database.ref('gameState');
     gameStateRef.on("value",function(data){
-       gameState = data.val();
-    })
+       gameState = data.val()
+    });
     
   }
 
@@ -19,11 +19,11 @@ class Game {
     if(gameState === 0){
       player = new Player();
       var playerCountRef=await database.ref('playerCount').once("value");
-      if (playerCountRef.exist()){
+      if (playerCountRef.exists()){
         playerCount=playerCountRef.val();
         player.getCount();
       }
-      form = new Form()
+      form = new Form();
       form.display();
     }
   }
@@ -33,17 +33,24 @@ class Game {
     textSize(30);
     text("GAME START",120,100);
     Player.getPlayerInfo();
+
     if (allPlayer!==undefined){
       var display_Position = 130;
-      display_Position += 20;
-      textSize(15);
-      text(allPlayer[plr].name+":"+allPlayer[plr].distance,120,display_Position);
+        for (var plr in allPlayer){
+          if (plr==="Player"+player.index)
+            fill ("red");
+          else 
+            fill ("black");
+          display_Position += 20;
+          textSize(15);
+          text(allPlayer[plr].name+":"+allPlayer[plr].distance,120,display_Position);
+        }
     }
-    
+
+    if (keyIsDown(UP_ARROW) && player.index!==null){
+      player.distance += 50;
+      player.update();
+    }
   }
 
-  if (KeyIsDown(UP_ARROW) && player.index!==null){
-    player.distance += 50;
-    player.update();
-  }
 }
